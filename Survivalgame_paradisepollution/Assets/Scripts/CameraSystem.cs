673af3f7933f3185ec4a8b1b3f300aaa5407bdc7
Saladsnek <505188@chrome.escg.ac.uk>
2020-03-03 16:33:17 +0000

    Camera follows player, short temp day added, day counter added, Fatigue begun to be added, bed for sleep added.

---
+++ Survivalgame_paradisepollution/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    public GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+            float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+            gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+}
---
+++ Survivalgame_paradisepollution/Assets/Scripts/MechanicsForPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MechanicsForPlayer : MonoBehaviour
+{
+    private float timeInDay = 20;
+    public int daysPassed = 0;
+    public float fatigue = 100;
+    public GameObject timeInDayUI;
+    public GameObject daysPassedUI;
+    public GameObject fatigueUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(timeInDay);
+        //Debug.Log(daysPassed);
+        timeInDay -= Time.deltaTime;
+        timeInDayUI.gameObject.GetComponent<Text>().text = ("TIME: " + (int)timeInDay);
+        daysPassedUI.gameObject.GetComponent<Text>().text = ("DAY: " + daysPassed);
+        fatigueUI.gameObject.GetComponent<Text>().text = ("FATIGUE: " + fatigue);
+        if (timeInDay <= 10.0f)
+        {
+            fatigue -= 50;
+        }
+        if (timeInDay <= 0.1f)
+        {
+            timeInDay += 20.0f;
+            daysPassed += 1;
+            if (fatigue <= 50)
+            {
+                fatigue += 50;
+            }
+        } 
+    }
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("touched bed");
+        timeInDay = 0.1f;
+    }
+}
